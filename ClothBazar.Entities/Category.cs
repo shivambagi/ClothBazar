155ce5d8f6cc5977a920ecb69365e4f3a243a3aa
shivambagi <84464752+shivambagi@users.noreply.github.com>
2022-01-15 13:36:29 +0530

    1ST Video - 1) Added Entities Projects(Products,Category) 2) Added Database project (Entity Framwk)(enabled,added and updated migartions)

---
+++ ClothBazar.Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Entities
+{
+    public class Category : BaseEntity
+    {
+        //ID,Name, Desc are inherited from BaseEntity class which are common to both Product and Category
+        public List<Product> Products { get; set; }
+
+    }
+}
---
+++ ClothBazar.Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Entities
+{
+    public class Product : BaseEntity
+    {
+        //ID,Name, Desc are inherited from BaseEntity class which are common to both Product and Category
+        public decimal Price { get; set; }
+        public Category Category { get; set; }
+    }
+}
